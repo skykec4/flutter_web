bdff820a556c2e06508a52eaa8a1c5ba8b14db9e
skykec4 <skykec4@naver.com>
2021-03-06 00:27:28 +0900

    다시 삭제

--- skykec4.github.io/android/app/src/main/kotlin/com/eungguri/flutter_blog/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.eungguri.flutter_blog
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- skykec4.github.io/lib/ui/utils/constant.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-
-import 'package:flutter/material.dart';
-
-class Constant {
-  Color main = Colors.red;
-}
-
-final colorMain = Color(0xFFDE5095);
