9072f61735c1b1e83203377c73906dea8b5a8bc4
skykec4 <skykec4@naver.com>
2021-03-05 23:43:10 +0900

    test

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -4,7 +4,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('home'),
+      child: Text('home1'),
     );
   }
 }
