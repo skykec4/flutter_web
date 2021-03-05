90113f17e67597ad372ee7a6bec77043f9ecca4a
skykec4 <skykec4@naver.com>
2021-03-06 00:25:07 +0900

    ㅁㄴㅇ

---
+++ skykec4.github.io/android/app/src/main/kotlin/com/eungguri/flutter_blog/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.eungguri.flutter_blog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ skykec4.github.io/lib/ui/utils/constant.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Constant {
+  Color main = Colors.red;
+}
+
+final colorMain = Color(0xFFDE5095);
